dc75a1d54370919e9eb0b0f9f90f994459612add
Justanis <anismokhtari32@gmail.com>
2026-05-08 03:23:51 +0100

    fixing

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ print("KEY:", os.getenv("SUPABASE_KEY"))
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
 
 supabase: Client = create_client(
     os.getenv("SUPABASE_URL"),
